e94eb0d3345f3d9235c30133aeb72b33ce45e97a
Levy <xulongwu@kiwik.cn>
2018-01-18 10:18:59 +0800

    Not to draw x axis separator when that label is null

--- PNChart/PNLineChart.m
+++ PNChart/PNLineChart.m
@@ -772,6 +772,10 @@ andProgressLinePathsColors:(NSMutableArray *)progressLinePathsColors {
             // draw x axis separator
             CGPoint point;
             for (NSUInteger i = 0; i < [self.xLabels count]; i++) {
+                NSString *xlabel = self.xLabels[i];
+                if (xlabel.length == 0) {
+                    continue;
+                }
                 point = CGPointMake(2 * _chartMarginLeft + (i * _xLabelWidth), _chartMarginBottom + _chartCavanHeight);
                 CGContextMoveToPoint(ctx, point.x, point.y - 2);
                 CGContextAddLineToPoint(ctx, point.x, point.y);
